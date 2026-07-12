53d052b9b6a2452a147fb2bfc803466ef557b86d
Subhankar <subhankardhal637@gmail.com>
2026-07-13 00:00:27 +0530

    using nested if

--- HTML_CSS_JS/LeapYearChecker/script.js
+++ HTML_CSS_JS/LeapYearChecker/script.js
@@ -1,10 +1,43 @@
+// function checkLeapYear() {
+
+//     let year = Number(document.getElementById("year").value);
+
+//     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+//         document.getElementById("result").innerHTML =
+//             year + " is a Leap Year";
+//     }
+//     else {
+//         document.getElementById("result").innerHTML =
+//             year + " is Not a Leap Year";
+//     }
+
+// }
+
+// Using Nested if
+
 function checkLeapYear() {
 
     let year = Number(document.getElementById("year").value);
 
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        document.getElementById("result").innerHTML =
-            year + " is a Leap Year";
+    if (year % 4 == 0) {
+
+        if (year % 100 == 0) {
+
+            if (year % 400 == 0) {
+                document.getElementById("result").innerHTML =
+                    year + " is a Leap Year";
+            }
+            else {
+                document.getElementById("result").innerHTML =
+                    year + " is Not a Leap Year";
+            }
+
+        }
+        else {
+            document.getElementById("result").innerHTML =
+                year + " is a Leap Year";
+        }
+
     }
     else {
         document.getElementById("result").innerHTML =
