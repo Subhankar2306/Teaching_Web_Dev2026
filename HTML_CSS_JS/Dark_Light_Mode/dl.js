2b8e2c707724f455d841883c4be91dc498b01915
Subhankar <subhankardhal637@gmail.com>
2026-07-23 11:22:51 +0530

    Some modification

---
+++ HTML_CSS_JS/Dark_Light_Mode/dl.js
@@ -0,0 +1,7 @@
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+// element.classList.add("dark-mode");      // Add class
+// element.classList.remove("dark-mode");   // Remove class
+// element.classList.toggle("dark-mode");   // Add/Remove automatically
